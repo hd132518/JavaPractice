4603ee98d2f7b0fe95b1e6de6dec7d1addc538a2
Park Geon <Park Geon@172.17.3.18>
2015-04-20 12:23:57 +0900

    zfrejskgjferogj

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -15,6 +15,7 @@ public class CarTest {
 
 	public static void main (String[] args) {
 
+		/*
 		Car car1 = new Car();
 		car1.setModel("제네시스");
 		car1.setMaxSpeed(225);
@@ -40,6 +41,31 @@ public class CarTest {
 		car1.print();
 		car2.print();
 		car3.print();
+		*/
+		/*
+		Car2 car1 = new Car2("현대자동차", "제네시스", "점정색", 225, 50000000);
 
+		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+
+		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 3800000);
+
+		System.out.println("<< 자동차 목록 >>");
+		car1.print();
+		car2.print();
+		car3.print();
+		*/
+		
+		Car3 car1 = new Car3("현대자동차", "제네시스", "점정색", 225, 50000000);
+
+		Car3 car2 = new Car3("기아자동차", "K7", "흰색", 246);
+
+		Car3 car3 = new Car3("삼성자동차", "SM7", "회색");
+
+		System.out.println("<< 자동차 목록 >>");
+		car1.print();
+		car2.print();
+		car3.print();
+		
+		
 	}
 }
