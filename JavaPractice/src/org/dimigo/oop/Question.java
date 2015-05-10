9d23f725a91efac70b0b1f7839159ae65ccf2903
Park Geon <Park Geon@172.17.3.26>
2015-05-11 02:24:04 +0900

    k

--- JavaPractice/src/org/dimigo/oop/Question.java
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -30,6 +30,7 @@ public class Question {
 			System.out.println("틀렸습니다. 정답은 18입니다.");
 			wrongcount++;
 		}
+		System.out.println();
 		scanner.nextLine();
 		
 		System.out.println("자바에서 문자열을 다루기 위한 클래스를 구하여라.");
@@ -38,19 +39,23 @@ public class Question {
 			System.out.println("정답입니다.");
 		} else if(wrongcount == 0) {
 			System.out.println("틀렸습니다. 정답은 String입니다.");
-			
+			wrongcount++;
 		} else {
 			System.out.println("또 틀렸습니다. 아는게 뭡니까.");
 			wrongcount++;
 		}
+		System.out.println();
 		
 		System.out.println("대한민국의 정식 국호를 구하여라.");
 		String name = scanner.nextLine();
 		if("대한민국".equals(name)) {
 			System.out.println("정답입니다. 당신은 애국자입니다.");
+		} else if(wrongcount == 2) {
+			System.out.println("틀렸습니다. 당신은 아는 게 없는 매국노입니다.");
 		} else {
 			System.out.println("틀렸습니다. 당신은 매국노입니다.");
 		}
+		System.out.println();
 		
 		scanner.close();
  	}
